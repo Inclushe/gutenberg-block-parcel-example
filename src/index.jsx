102d706152586e9f96b2b469ec0d113a73d07e81
Ethan <ethan@inclushe.com>
2022-05-31 16:57:32 -0400

    Added code for gutenpride example

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,32 @@
-// Register the block
-window.wp.blocks.registerBlockType( 'gutenberg-examples/example-01-basic-esnext', {
-    edit: function () {
-        return <p> Hello world (from the editor)</p>;
+ /**
+ * Internal dependencies
+ */
+const registerBlockType = window.wp.blocks.registerBlockType
+
+import Edit from './edit';
+import save from './save';
+ 
+ /**
+  * Every block starts by registering a new block type definition.
+  *
+  * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
+  */
+registerBlockType('gutenberg-parcel-example/gutenpride', {
+    /**
+     * Used to construct a preview for the block to be shown in the block inserter.
+     */
+    example: {
+        attributes: {
+            message: 'Parcel',
+        },
     },
-    save: function () {
-        return <p> Hola mundo (from the frontend) </p>;
-    },
-} );
+    /**
+     * @see ./edit.js
+     */
+    edit: Edit,
+    /**
+     * @see ./save.js
+     */
+    save,
+});
+ 
